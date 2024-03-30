1490353061e97f35d285d5c529a1df3c036b5436
sk <mrrkhann9u@email.com>
2024-03-30 23:25:01 +0700

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://localhost:3000',
+      url: 'https://hm008-web-nextjs-seo.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://localhost:3000/about',
+      url: 'https://hm008-web-nextjs-seo.vercel.app/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://localhost:3000/service',
+      url: 'https://hm008-web-nextjs-seo.vercel.app/service',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
